ed67273bdc56e0294df432e776f7b9d50f187b95
jeffreyjeffreywang <jeffjeffreywang@gmail.com>
2025-10-31 16:20:59 -0700

    Introduce generic multimodal preparation (draft)
    Signed-off-by: jeffreyjeffreywang <jeffjeffreywang@gmail.com>

--- python/ray/llm/_internal/batch/processor/vllm_engine_proc.py
+++ python/ray/llm/_internal/batch/processor/vllm_engine_proc.py
@@ -196,7 +196,7 @@ def build_vllm_engine_processor(
     )
     if tokenize_stage_cfg.enabled:
         stages.append(
-            TokenizeStage(
+            ChatTemplateStage(
                 fn_constructor_kwargs=dict(
                     model=tokenize_stage_cfg.model_source,
                 ),
